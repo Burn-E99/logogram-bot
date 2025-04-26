5b87cfda751d1532ea1a63a9805e4bb0e7f2fdba
Ean Milligan <ean.milligan@gmail.com>
2025-04-25 22:06:08 -0400

    deno fmt

--- deps.ts
+++ deps.ts
@@ -1,12 +1,5 @@
-export {
-	startBot, editBotStatus, editBotNickname,
-	sendMessage,
-	Intents, DiscordActivityTypes,
-	cache, cacheHandlers
-} from "https://deno.land/x/discordeno@12.0.1/mod.ts";
+export { cache, cacheHandlers, DiscordActivityTypes, editBotNickname, editBotStatus, Intents, sendMessage, startBot } from 'https://deno.land/x/discordeno@12.0.1/mod.ts';
 
-export type {
-	DiscordenoMessage, DiscordenoGuild, Embed
-} from "https://deno.land/x/discordeno@12.0.1/mod.ts";
+export type { DiscordenoGuild, DiscordenoMessage, Embed } from 'https://deno.land/x/discordeno@12.0.1/mod.ts';
 
 export { initLog, log, LogTypes as LT } from 'https://raw.githubusercontent.com/Burn-E99/Log4Deno/V1.1.1/mod.ts';
